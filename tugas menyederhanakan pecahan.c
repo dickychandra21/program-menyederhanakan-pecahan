faefca1ff0345f852cc73ab7b5922cd65997cff3
dickychandra21 <45659402+dickychandra21@users.noreply.github.com>
2018-12-18 18:43:31 +0700

    Add files via upload

---
+++ tugas menyederhanakan pecahan.c
@@ -0,0 +1,22 @@
+#include<conio.h>
+int perulangan(int n)
+int main(void)
+{
+	
+int n;
+printf("PROGRAM PERULANGAN KALIMAY SEBANYAK (n) KALI\n\n");
+printf("masukan nilai (n) :");
+scanf("%d,&n");
+perulangan(n);
+getchar ();
+}
+int pengulangan(int n)
+	
+{
+int i;
+for(i=1;i<=n;i++)
+printf("algoritma dan program\n");
+return(i);
+
+}
+
